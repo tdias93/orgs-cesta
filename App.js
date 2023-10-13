3ddcac05e5792116b5aaa839ce0662ff69922e20
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2023-10-13 19:49:22 -0300

    Dados para o app

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import { Cesta } from './src/pages/Cesta/Cesta';
+import mockCesta from './src/mocks/MockCestas'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fonteCarregadas] = useFonts({
+    'MontserratRegular': Montserrat_400Regular,
+    'MontserratBold': Montserrat_700Bold
+  });
+
+  if (!fonteCarregadas) {
+    return SplashScreen.hideAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <Cesta {...mockCesta }/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
